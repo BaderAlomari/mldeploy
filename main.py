d1e259b479c85e4131f2b1c70f029098896633a8
BaderAlomari <132921423+BaderAlomari@users.noreply.github.com>
2025-04-11 22:50:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Put the code for your API here.
+# Put the codee for your API here.
 from pydantic import BaseModel
 from ml.data import process_data
 import uvicorn
@@ -24,90 +24,26 @@ class features(BaseModel):
     hours_per_week: int
     native_country: str
 
-    model_config = { "json_schema_extra" : {
-            "examples": [
-                {
-                    "age": 39,
-                    "workclass": "State-gov",
-                    "fnlgt": 77516,
-                    "education": "Bachelors",
-                    "education_num": 13,
-                    "marital_status": "Never-married",
-                    "occupation": "Adm-clerical",
-                    "relationship": "Not-in-family",
-                    "race": "White",
-                    "sex": "Male",
-                    "capital_gain": 2174,
-                    "capital_loss": 0,
-                    "hours_per_week": 40,
-                    "native_country": "United-States"
-                },
-                {
-                    "age": 28,
-                    "workclass": "Private",
-                    "fnlgt": 338409,
-                    "education": "Bachelors",
-                    "education_num": 13,
-                    "marital_status": "Married-civ-spouse",
-                    "occupation": "Prof-specialty",
-                    "relationship": "Wife",
-                    "race": "Black",
-                    "sex": "Female",
-                    "capital_gain": 0,
-                    "capital_loss": 0,
-                    "hours_per_week": 40,
-                    "native_country": "Cuba"
-                },
-                {
-                    "age": 52,
-                    "workclass": "Self-emp-not-inc",
-                    "fnlgt": 209642,
-                    "education": "HS-grad",
-                    "education_num": 9,
-                    "marital_status": "Married-civ-spouse",
-                    "occupation": "Exec-managerial",
-                    "relationship": "Husband",
-                    "race": "White",
-                    "sex": "Male",
-                    "capital_gain": 0,
-                    "capital_loss": 0,
-                    "hours_per_week": 45,
-                    "native_country": "United-States"
-                },
-                {
-                    "age": 31,
-                    "workclass": "Private",
-                    "fnlgt": 45781,
-                    "education": "Masters",
-                    "education_num": 14,
-                    "marital_status": "Never-married",
-                    "occupation": "Prof-specialty",
-                    "relationship": "Not-in-family",
-                    "race": "White",
-                    "sex": "Female",
-                    "capital_gain": 14084,
-                    "capital_loss": 0,
-                    "hours_per_week": 50,
-                    "native_country": "United-States"
-                },
-                {
-                    "age": 40,
-                    "workclass": "Private",
-                    "fnlgt": 193524,
-                    "education": "Doctorate",
-                    "education_num": 16,
-                    "marital_status": "Married-civ-spouse",
-                    "occupation": "Prof-specialty",
-                    "relationship": "Husband",
-                    "race": "White",
-                    "sex": "Male",
-                    "capital_gain": 0,
-                    "capital_loss": 0,
-                    "hours_per_week": 60,
-                    "native_country": "United-States"
-                }
-            ]
-        }}
+    model_config = { 
+    "json_schema_extra": [
+        {
+            "age": 39,
+            "workclass": "State-gov",
+            "fnlgt": 77516,
+            "education": "Bachelors",
+            "education_num": 13,
+            "marital_status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Male",
+            "capital_gain": 2174,
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States"
+        }
+    ]
+}
 
 app = FastAPI()
 
